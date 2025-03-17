44bd1d3781984fb7436e23c9adebfad677336e4a
Alex Belov <alex@cornel.su>
2025-03-17 18:36:49 +0300

    Initial Commit

---
+++ ChickensFarmCrash/ChickensFarmCrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChickensFarmCrashApp.swift
+//  ChickensFarmCrash
+//
+//  Created by alex on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ChickensFarmCrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChickensFarmCrashTests/ChickensFarmCrashTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChickensFarmCrashTests.swift
+//  ChickensFarmCrashTests
+//
+//  Created by alex on 3/17/25.
+//
+
+import Testing
+@testable import ChickensFarmCrash
+
+struct ChickensFarmCrashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
